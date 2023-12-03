7596cd98572a55a922c3aa8d88ab3e3aae160b84
izik.avinoam <izikavinoam@gmail.com>
2023-12-03 23:30:01 +0200

    recode

--- Day1/main.cpp
+++ Day1/main.cpp
@@ -2,41 +2,49 @@
 // Created by izik on 3/12/23
 //
 #include <iostream>
-#include <sstream>
 #include <string>
 #include <fstream>
+#include <vector>
 
-int first_digit(const std::string &str)
+std::vector<std::string> read_lines(const std::string &file_name)
 {
-    size_t inx = str.find_first_of("0123456789");
-    return (str.at(inx) - '0');
-}
-
-int last_digit(const std::string &str)
-{
-    size_t inx = str.find_last_of("0123456789");
-    return (str.at(inx) - '0');
-}
-
-int combain_digits(const std::string &str)
-{
-    return ((first_digit(str)*10) + last_digit(str));
-}
-
-int main()
-{
+    std::vector<std::string> input;
+    std::ifstream inputf(file_name);
     std::string line;
-    std::ifstream inputf("../input");
-    int sum = 0;
     if (!inputf.is_open())
-        std::cout << "failed to open " << "input" << '\n';
+        std::cout << "failed to open " << file_name << std::endl;
     else
     {
         while (std::getline(inputf, line))
         {
-            sum+=combain_digits(line);
+            input.push_back(line);
         }
-        std::cout << sum << std::endl;
     }
+    return input;
+}
+
+int get_number(const std::string &str)
+{
+    size_t first_inx = str.find_first_of("0123456789");
+    int first_digi = str.at(first_inx) - '0';
+    size_t last_inx = str.find_last_of("0123456789");
+    int last_digi = str.at(last_inx) - '0';
+    return ((first_digi*10) + last_digi);
+}
+
+int sum_of_data(std::vector<std::string> data)
+{
+    int sum = 0;
+    for (auto i: data)
+    {
+        sum += get_number(i);
+    }
+    return sum;
+}
+
+int main()
+{
+    std::vector<std::string> data(read_lines("../input"));
+    std::cout << sum_of_data(data) << std::endl;
     return 0;
 }
