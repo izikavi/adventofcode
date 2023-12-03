e8529aa42c2a104c71e0977e95a631c0bc477903
izik.avinoam <izikavinoam@gmail.com>
2023-12-03 22:11:12 +0200

    add input file & start codding

--- 03-12-2023/main.cpp
+++ 03-12-2023/main.cpp
@@ -1,3 +1,43 @@
 //
-// Created by izik on 12/3/23.
+// Created by izik on 3/12/23.
 //
+#include <iostream>
+#include <sstream>
+#include <string>
+#include <fstream>
+
+unsigned int first_digit(const std::string str)
+{
+    size_t inx = str.find_first_of("0123456789");
+    int digi = (str.at(inx) - '0');;
+    return (str.at(inx) - '0');
+}
+
+unsigned int last_digit(const std::string srt)
+{
+    unsigned int digit = 0;
+    return digit;
+}
+
+unsigned int combain_digits(const std::string str)
+{
+    return first_digit(str) + last_digit(str);
+}
+
+int main()
+{
+    std::string line;
+    std::ifstream inputf("input");
+    int sum = 0;
+    if (!inputf.is_open())
+        std::cout << "failed to open " << "input" << '\n';
+    else
+    {
+        while (std::getline(inputf, line))
+        {
+            sum+=combain_digits(line);
+        }
+        std::cout << sum << std::endl;
+    }
+    return 0;
+}
