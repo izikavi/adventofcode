4690f7eaffc1f84a759a0331ea53a998dd259fc0
izik.avinoam <izikavinoam@gmail.com>
2023-12-05 15:06:10 +0200

    start build day3-part1

--- Day3/day3-part1.cpp
+++ Day3/day3-part1.cpp
@@ -8,6 +8,8 @@
 
 using namespace std;
 
+const string NOT_SYMBOL = "0123456789.";
+
 vector<string> read_lines(const string &file_name)
 {
     vector<string> input;
@@ -27,6 +29,60 @@ vector<string> read_lines(const string &file_name)
     return input;
 }
 
+int get_num()
+{
+    int num = 0;
+    return num;
+}
+
+vector<vector<bool> > set_matrix(const vector<string> &data)
+{
+    int height = data.size();
+    int width = data.at(0).length();
+    vector<vector<bool> > matirx;
+    for (int row=0; row < height; ++row)
+    {
+        for (int col=0; col < width; ++col)
+        {
+            matirx[row][col] = false;
+        }
+    }
+    for (int row=0; row < height; ++row)
+    {
+        for (int col=0; col < width; ++col)
+        {
+            if (string::npos == NOT_SYMBOL.find_first_not_of(data.at(row).at(col)))
+            {
+                if (row && col)
+                    matirx.at(row - 1).at(col - 1) = true;
+                if ((row < (height-1)) && col)
+                    matirx.at(row + 1).at(col - 1) = true;
+                if (row && (col < (width-1)))
+                    matirx.at(row - 1).at(col + 1) = true;
+                if ((row < (height-1)) && (col < (width -1)))
+                    matirx.at(row + 1).at(col + 1) = true;
+            }
+        }
+    }
+    return matirx;
+}
+
+int get(const vector<vector<bool> > &matrix, const vector<string> &data)
+{
+    int sum = 0;
+    int height = data.size();
+    int width = data.at(0).length();
+    for (int row=0; row < height; ++row)
+    {
+        for (int col=0; col < width; ++col)
+        {
+            if (matrix.at(row).at(col))
+            {
+                sum += get_num()
+            }
+        }
+    }
+}
 
 int main()
 {
